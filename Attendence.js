3d98adb2a8ea5196383183fb169893c8a8ce51f7
unknown <abhisheksrivastava9719@gmail.com>
2022-08-04 23:00:17 +0530

    [Abhishek] Added daily wage functionality

--- Attendence.js
+++ Attendence.js
@@ -10,3 +10,25 @@ else
 {
     console.log("Employee is Present");
 }
+
+const PartTime=1;
+const FullTime=2;
+const PartTimeHrs=4;
+const FullTimeHrs=8;
+const WagePerHr=20;
+
+let empHrs=0;
+empCheck=Math.floor(Math.random()*3);
+switch(empCheck)
+{
+    case PartTime:
+        empHrs=PartTimeHrs;
+        break;
+    case FullTime:
+        empHrs=FullTimeHrs;
+        break;
+    default:
+        empHrs=0;
+}
+let EmpWage=empHrs*WagePerHr;
+console.log("Emp Wage = "+EmpWage);
