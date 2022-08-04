06d065112fe46b0e4a1698ae805861bb82de895d
unknown <abhisheksrivastava9719@gmail.com>
2022-08-04 22:51:20 +0530

    [Abhishek]Added the attendence function

--- Attendence.js
+++ Attendence.js
@@ -1,1 +1,12 @@
 console.log("Welcome to the employee wage problem using javascript");
+//UC1
+const absent=0
+let empCheck=Math.floor(Math.random()*2);
+if(empCheck==absent)
+{   console.log("Employee is ABSENT");
+    return;
+}
+else
+{
+    console.log("Employee is Present");
+}
